5fbef100e3631e2cee8e9bebfdec48ebcfdbcd74
olya <olgastet01032005@gmail.com>
2025-02-13 10:20:58 +0200

    hw

--- 11.02/MainWindow.xaml.cs
+++ 11.02/MainWindow.xaml.cs
@@ -24,14 +24,21 @@ namespace hw
 
         private void thread_first()
         {
+            mutex_file.WaitOne();
+
+            Thread thread_2 = new Thread(thread_second);
+            thread_2.Start();
+
             Dispatcher.Invoke(() =>
             {
                 status_1.Text = "thread 1 is generating random numbers";
                 progress_1.Value = 0;
             });
+
             Random random = new Random();
             int count_numbers = 500;
             List<int> numbers = new List<int>();
+
             for (int i = 0; i < count_numbers; i++)
             {
                 int num = random.Next(1, 1000);
@@ -40,16 +47,17 @@ namespace hw
                 Dispatcher.Invoke(() => { progress_1.Value = prog; });
                 Thread.Sleep(10);
             }
-            mutex_file.WaitOne();
-            Thread thread_2 = new Thread(thread_second);
-            thread_2.Start();
+
             try
             {
                 using (StreamWriter writer = new StreamWriter("numbers.dat"))
                 {
                     foreach (int num in numbers)
+                    {
                         writer.WriteLine(num);
+                    }
                 }
+
                 Dispatcher.Invoke(() => { status_1.Text = $"thread 1 generated {count_numbers} numbers in file"; });
             }
             catch (Exception ex)
@@ -64,29 +72,18 @@ namespace hw
 
         private void thread_second()
         {
-            while (!File.Exists("numbers.dat"))
-            {
-                Thread.Sleep(50);
-            }
-            while (true)
-            {
-                int lineCount = 0;
-                try { lineCount = File.ReadAllLines("numbers.dat").Length; } catch { }
-                if (lineCount >= 500)
-                    break;
-                Thread.Sleep(50);
-            }
             Dispatcher.Invoke(() =>
             {
-                status_2.Text = "waiting to mutex";
+                status_2.Text = "thread 2 waiting for mutex";
                 progress_2.Value = 0;
             });
+
             mutex_file.WaitOne();
-            Dispatcher.Invoke(() =>
-            {
-                status_2.Text = "thread 2 is filtering prime numbers";
-            });
+
+            Dispatcher.Invoke(() => { status_2.Text = "thread 2 is filtering prime numbers"; });
+
             List<int> numbers = new List<int>();
+
             try
             {
                 using (StreamReader sr = new StreamReader("numbers.dat"))
@@ -103,29 +100,32 @@ namespace hw
             {
                 Dispatcher.Invoke(() => { status_2.Text = "Error with reading: " + ex.Message; });
             }
-            finally
-            {
-                mutex_file.ReleaseMutex();
-            }
+
+            Thread thread_3 = new Thread(thread_third);
+            thread_3.Start();
+
             List<int> primes = new List<int>();
             int total = numbers.Count;
+
             for (int i = 0; i < total; i++)
             {
                 int num = numbers[i];
-                if (prime_num(num)) primes.Add(num);
+                if (prime_num(num))
+                    primes.Add(num);
+
                 int prog = (i + 1) * 100 / total;
                 Dispatcher.Invoke(() => { progress_2.Value = prog; });
                 Thread.Sleep(5);
             }
-            mutex_file.WaitOne();
-            Thread thread_3 = new Thread(thread_third);
-            thread_3.Start();
+
             try
             {
                 using (StreamWriter sw = new StreamWriter("primes_num.dat"))
                 {
-                    foreach (int num in primes) sw.WriteLine(num);
+                    foreach (int num in primes)
+                        sw.WriteLine(num);
                 }
+
                 Dispatcher.Invoke(() => { status_2.Text = $"thread 2 found {primes.Count} prime numbers in file"; });
             }
             catch (Exception ex)
@@ -140,31 +140,17 @@ namespace hw
 
         private void thread_third()
         {
-            while (!File.Exists("primes_num.dat"))
-            {
-                Thread.Sleep(50);
-            }
-            while (true)
-            {
-                try
-                {
-                    if (new FileInfo("primes_num.dat").Length > 0)
-                        break;
-                }
-                catch { }
-                Thread.Sleep(50);
-            }
             Dispatcher.Invoke(() =>
             {
-                status_3.Text = "waiting to mutex";
+                status_3.Text = "thread 3 waiting for mutex";
                 progress_3.Value = 0;
             });
             mutex_file.WaitOne();
-            Dispatcher.Invoke(() =>
-            {
-                status_3.Text = "thread 3 is filtering primes ending with 7";
-            });
+
+            Dispatcher.Invoke(() => { status_3.Text = "thread 3 is filtering primes ending with 7"; });
+
             List<int> primes = new List<int>();
+
             try
             {
                 using (StreamReader sr = new StreamReader("primes_num.dat"))
@@ -181,28 +167,33 @@ namespace hw
             {
                 Dispatcher.Invoke(() => { status_3.Text = "Error: " + ex.Message; });
             }
-            finally
-            {
-                mutex_file.ReleaseMutex();
-            }
+
             List<int> primes7 = new List<int>();
             int total = primes.Count;
+
             for (int i = 0; i < total; i++)
             {
                 int num = primes[i];
-                if (num % 10 == 7) primes7.Add(num);
+                if (num % 10 == 7)
+                    primes7.Add(num);
+
                 int prog = (i + 1) * 100 / total;
                 Dispatcher.Invoke(() => { progress_3.Value = prog; });
                 Thread.Sleep(5);
             }
-            mutex_file.WaitOne();
+
             try
             {
                 using (StreamWriter sw = new StreamWriter("seven_numbers.dat"))
                 {
-                    foreach (int num in primes7) sw.WriteLine(num);
+                    foreach (int num in primes7)
+                        sw.WriteLine(num);
                 }
-                Dispatcher.Invoke(() => { status_3.Text = $"thread 3 found {primes7.Count} numbers ending with 7 in file"; });
+
+                Dispatcher.Invoke(() =>
+                {
+                    status_3.Text = $"thread 3 found {primes7.Count} numbers ending with 7 in file";
+                });
             }
             catch (Exception ex)
             {
@@ -218,7 +209,11 @@ namespace hw
         {
             if (n < 2) return false;
             for (int i = 2; i <= Math.Sqrt(n); i++)
-                if (n % i == 0) return false;
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
             return true;
         }
     }
--- 11.02/obj/Debug/net8.0-windows/11.02.AssemblyInfo.cs
+++ 11.02/obj/Debug/net8.0-windows/11.02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("11.02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeaafc666fdd471c44dc4e163bd9be1f913d2e28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0025bc24a07d43977c18ca0743e8f21846d6e33")]
 [assembly: System.Reflection.AssemblyProductAttribute("11.02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("11.02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
